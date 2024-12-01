b7674395de5743658c1e71cfc5e4a2811d93245f
ds-challange <d@vidson.email>
2024-11-30 23:12:28 -0300

    feat(db): initialize database connection and refactor main application flow

--- src/configs/beanie_config.py
+++ src/configs/beanie_config.py
@@ -1,9 +1,25 @@
 import os
 from typing import Any
 
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from src.configs.beanie_config import MONGODB_URL, AIRLINE_DOCUMENT_MODELS
+from src.modules.airline.contracts.implementations.beanie.document import AirlineDocument
+
+
 
 AIRLINE_DOCUMENT_MODELS: list[Any] = [
+    AirlineDocument
 ]   
 
 
 MONGODB_URL = os.getenv("MONGODB_URL")
+
+async def init_db():
+    """
+    Initialize the database connection
+    """
+    
+    airline_database = AsyncIOMotorClient(MONGODB_URL).airline  # type: ignore
+    await init_beanie(airline_database, document_models=AIRLINE_DOCUMENT_MODELS)
