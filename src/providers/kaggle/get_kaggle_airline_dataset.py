12733753635251eba9e6621903a8b9490ecbfd94
ds-challange <d@vidson.email>
2024-11-30 23:12:47 -0300

    feat(kaggle): add docstrings for dataset retrieval functions

--- src/providers/kaggle/get_kaggle_airline_dataset.py
+++ src/providers/kaggle/get_kaggle_airline_dataset.py
@@ -1,11 +1,14 @@
 import kaggle
-import os
 
 from src.configs.kaggle_config import KAGGLE_DATASET, KAGGLE_OWNER, KAGGLE_URL
 from src.providers.kaggle.contracts.dtos.kaggle_dtos import KaggleDownloadFilesDto
 
 
 async def find_file_names()-> list[str]:
+    """
+    Find the file names of the kaggle dataset
+    """
+    
     list_dto_names = []
     datasets_describe_list_files:list[KaggleDownloadFilesDto] = (
         kaggle.api.datasets_list_files(
@@ -18,6 +21,16 @@ async def find_file_names()-> list[str]:
     return list_dto_names
     
 async def get_kaggle_airline_dataset_zip(file_name: str) -> tuple[str, bool]:
+    """
+    Download the kaggle dataset zip file
+
+    Args:
+        file_name (str): The file name of the kaggle dataset
+
+    Returns:
+        tuple[str, bool]: The file name and if the download is completed
+    """
+    
     download_is_completed = (
         kaggle.api.dataset_download_file(
             dataset=KAGGLE_URL,
---
+++ src/utils/unzip_files.py
@@ -0,0 +1,30 @@
+
+import os
+import zipfile
+
+
+async def unzip_file(file_name: str,file_path:str, extract_to: str, delete_zip:bool = True) -> str:
+    """Unzip a file to a specific path
+
+    Args:
+        file_name (str): File name to unzip
+        file_path (str): File path to unzip
+        extract_to (str): Extract to path where the file will be unzip
+        delete_zip (bool, optional): Boolean to delete zip after extract. Defaults to True.
+
+    Returns:
+        str: Path of the unzip file
+    """
+    
+    os.makedirs(extract_to, exist_ok=True)
+
+    zip_file = file_path+"/"+file_name
+
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+        
+    if delete_zip:
+        os.remove(file_path)
+    
+    unzip_file_path = extract_to + "/" + file_name
+    return unzip_file_path
