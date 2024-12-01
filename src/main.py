2c73a7641fcbee0ee0c71067d3d35a4e1b29877b
ds-challange <d@vidson.email>
2024-11-30 23:14:21 -0300

    refactor(main): remove unused imports and clean up main execution flow

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 import asyncio
 
-from src.modules.airline.contracts.implementations.airline_etl.parse_csv_to_document import parse_csv_to_document
-from src.modules.airline.contracts.implementations.beanie.document import AirlineDocument
 from src.orquestrator import get_data
-from src.providers.kaggle.get_kaggle_airline_dataset import find_file_names, get_kaggle_airline_dataset_zip
 from src.configs.beanie_config import  init_db
-from src.utils.unzip_files import unzip_file
 
 
 if __name__ == "__main__":
@@ -24,13 +20,6 @@ if __name__ == "__main__":
             await get_data()
         except Exception as e:
             print(f"Error to get data:{e}")
-        
-
-        
-      
-        
                 
-                
-
 
 asyncio.run(main())
