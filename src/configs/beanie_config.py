120faf789c98d9dab64237c6080fbe65dcfe2158
ds-challange <d@vidson.email>
2024-12-01 22:10:42 -0300

    feat(config): add chunk size configuration and return status from init_db function

--- src/configs/beanie_config.py
+++ src/configs/beanie_config.py
@@ -13,11 +13,14 @@ AIRLINE_DOCUMENT_MODELS: list[Any] = [
 
 
 MONGODB_URL = os.getenv("MONGODB_URL")
+CHUNK_SIZE = os.getenv("CHUNK_SIZE") or 10000
 
-async def init_db():
+async def init_db()-> bool:
     """
     Initialize the database connection
     """
     
     airline_database = AsyncIOMotorClient(MONGODB_URL).airline  # type: ignore
     await init_beanie(airline_database, document_models=AIRLINE_DOCUMENT_MODELS)
+    
+    return True
