4e48813d371e8f15c9e68a9c26ac11ea2be6657a
ds-challange <d@vidson.email>
2024-12-01 22:10:58 -0300

    feat(airline): enhance Airline model with additional fields and descriptions feat(kaggle): refactor dataset file retrieval and improve download handling

--- src/providers/kaggle/get_kaggle_airline_dataset.py
+++ src/providers/kaggle/get_kaggle_airline_dataset.py
@@ -1,3 +1,4 @@
+import asyncio
 import kaggle
 
 from src.configs.kaggle_config import KAGGLE_DATASET, KAGGLE_OWNER, KAGGLE_URL
@@ -9,16 +10,19 @@ async def find_file_names()-> list[str]:
     Find the file names of the kaggle dataset
     """
     
-    list_dto_names = []
     datasets_describe_list_files:list[KaggleDownloadFilesDto] = (
         kaggle.api.datasets_list_files(
             owner_slug=KAGGLE_OWNER,
             dataset_slug=KAGGLE_DATASET
             )
         )
-    for dataset in datasets_describe_list_files:
-        list_dto_names.append(dataset.name)
-    return list_dto_names
+
+    dataset_files = datasets_describe_list_files.get('datasetFiles', [])
+    typed_files: list[KaggleDownloadFilesDto] = [KaggleDownloadFilesDto(**file) for file in dataset_files]
+
+    file_names = [file.name for file in typed_files]
+
+    return file_names
     
 async def get_kaggle_airline_dataset_zip(file_name: str) -> tuple[str, bool]:
     """
@@ -31,12 +35,15 @@ async def get_kaggle_airline_dataset_zip(file_name: str) -> tuple[str, bool]:
         tuple[str, bool]: The file name and if the download is completed
     """
     
+    print(f"Downloading {file_name} await...")
     download_is_completed = (
         kaggle.api.dataset_download_file(
             dataset=KAGGLE_URL,
-            path="temp/data",
+            path="/code/temp/data",
             file_name=file_name
             )
         )
-    return file_name, download_is_completed
+    if download_is_completed:
+        return file_name, download_is_completed
 
+asyncio.run(get_kaggle_airline_dataset_zip("2009.csv"))
