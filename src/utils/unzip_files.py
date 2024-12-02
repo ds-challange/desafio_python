cd92d41173c767d29732b6a2744889c5f983e964
ds-challange <d@vidson.email>
2024-12-01 22:11:03 -0300

    fix(unzip_files): correct zip file path and ensure proper deletion of zip files

--- src/utils/unzip_files.py
+++ src/utils/unzip_files.py
@@ -2,7 +2,6 @@
 import os
 import zipfile
 
-
 async def unzip_file(file_name: str,file_path:str, extract_to: str, delete_zip:bool = True) -> str:
     """Unzip a file to a specific path
 
@@ -18,13 +17,17 @@ async def unzip_file(file_name: str,file_path:str, extract_to: str, delete_zip:b
     
     os.makedirs(extract_to, exist_ok=True)
 
-    zip_file = file_path+"/"+file_name
+    zip_file = file_path+"/"+file_name+".zip"
 
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
         
     if delete_zip:
-        os.remove(file_path)
+        os.remove(zip_file)
     
     unzip_file_path = extract_to + "/" + file_name
     return unzip_file_path
+
+
+
+    
